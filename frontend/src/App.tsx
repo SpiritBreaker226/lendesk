e91c4740e4d627f91e3c1afc1f032752c3008a0f
Jason Stathopulos <jstathopulos@gmail.com>
2022-09-04 18:21:18 -0700

    Add Layout For Non-Auth Users
    Add the Layout for non-auth users so that the Sign-Up and Login
    components can render their content.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,9 @@
-import styled from 'styled-components'
-
 import { AppBody } from './AppBody'
 import { ThemeProvider } from './theme'
 
-const AppContainer = styled.div`
-  text-align: center;
-`
-
-const HeaderContainer = styled.div`
-  background: ${(props) => props.theme.background};
-  border-bottom: 1px solid ${(props) => props.theme.border};
-  padding: 0 12px;
-`
-
 const App = () => (
   <ThemeProvider>
-    <AppContainer>
-      <HeaderContainer>
-        <h1>Lendesk</h1>
-      </HeaderContainer>
-
-      <AppBody />
-    </AppContainer>
+    <AppBody />
   </ThemeProvider>
 )
 
--- frontend/src/AppBody/AppBody.tsx
+++ frontend/src/AppBody/AppBody.tsx
@@ -1,10 +1,33 @@
-import styled from 'styled-components'
 import { FC } from 'react'
+import styled from 'styled-components'
 
-const AppBodyConainer = styled.main`
-  margin: 0 16px 16px;
+const AppBodyConainer = styled.main.attrs<{ isAuth: boolean }>((props) => ({
+  isAuth: props.isAuth || false,
+}))<{ isAuth: boolean }>`
+  height: 100vh;
+  width: 100%;
+
+  ${(props) =>
+    !props.isAuth &&
+    `
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      background: ${props.theme.lendeskBlue};
+    `}}
+`
+
+const Content = styled.section`
+  background: ${(props) => props.theme.secondary};
+  border: 1px solid ${(props) => props.theme.secondary};
+  border-radius: 4px;
+  padding: 24px;
 `
 
 export const AppBody: FC = () => {
-  return <AppBodyConainer></AppBodyConainer>
+  return (
+    <AppBodyConainer isAuth={false}>
+      <Content>Content</Content>
+    </AppBodyConainer>
+  )
 }
