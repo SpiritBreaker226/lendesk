f8244872b2638067ae2686f264046a9633192ae4
Jason Stathopulos <jstathopulos@gmail.com>
2022-09-09 17:43:41 -0700

    Add Error Boundary Component
    Add Error Boundary Component to get any errors that have to happen and
    display them to the developer. They usually would be sent to an error
    handling service.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,19 +2,22 @@ import { BrowserRouter as Router } from 'react-router-dom'
 
 import { AppBody } from './AppBody'
 import { AppHead } from './Components'
+import ErrorBoundary from './Components/ErrorBoundary'
 import { AuthProvider } from './context'
 import { ThemeProvider } from './theme'
 
 const App = () => (
-  <ThemeProvider>
-    <AuthProvider>
-      <Router>
-        <AppHead />
+  <ErrorBoundary>
+    <ThemeProvider>
+      <AuthProvider>
+        <Router>
+          <AppHead />
 
-        <AppBody />
-      </Router>
-    </AuthProvider>
-  </ThemeProvider>
+          <AppBody />
+        </Router>
+      </AuthProvider>
+    </ThemeProvider>
+  </ErrorBoundary>
 )
 
 export default App
